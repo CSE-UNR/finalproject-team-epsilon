8bce6cb4e8380a8fd42f8b56cc2d6f12d3d30760
1101-Mogensen-Terron <158001644+1101-Mogensen-Terron@users.noreply.github.com>
2024-05-03 16:52:39 -0700

    Update images.c

--- images.c
+++ images.c
@@ -179,5 +179,119 @@ void editImage(int image[MAX_SIZE][MAX_SIZE], int size) {
     } while (choice != 6);
 }
 
+void cropImage(int image[MAX_SIZE][MAX_SIZE], int *size) {
+    int new_size;
+    int row_start, row_end, col_start, col_end;
+    int i, j;
+
+    printf("Enter the size of the cropped image: ");
+    scanf("%d", &new_size);
+
+    printf("Enter the starting row index: ");
+    scanf("%d", &row_start);
+    printf("Enter the ending row index: ");
+    scanf("%d", &row_end);
+    printf("Enter the starting column index: ");
+    scanf("%d", &col_start);
+    printf("Enter the ending column index: ");
+    scanf("%d", &col_end);
+
+
+    if (row_start < 0 || row_end >= *size || col_start < 0 || col_end >= *size || row_start > row_end || col_start > col_end) {
+        printf("Invalid crop indices.\n");
+        return;
+    }
+
+
+    *size = new_size;
+
+
+    for (i = 0; i < new_size; i++) {
+        for (j = 0; j < new_size; j++) {
+            image[i][j] = image[row_start + i][col_start + j];
+        }
+    }
+
+    printf("Image cropped successfully.\n");
+}
+
+
+void dimImage(int image[MAX_SIZE][MAX_SIZE], int size) {
+    int i, j;
+
+    for (i = 0; i < size; i++) {
+        for (j = 0; j < size; j++) {
+            if (image[i][j] > 1) { 
+                image[i][j]--; 
+            }
+        }
+    }
+
+    printf("Image dimmed successfully.\n");
+}
+
+
+void brightenImage(int image[MAX_SIZE][MAX_SIZE], int size) {
+    int i, j;
+
+    for (i = 0; i < size; i++) {
+        for (j = 0; j < size; j++) {
+            if (image[i][j] < 4) { 
+                image[i][j]++; 
+            }
+        }
+    }
+
+    printf("Image brightened successfully.\n");
+}
+
+
+void rotateImage90Degrees(int image[MAX_SIZE][MAX_SIZE], int size) {
+    int temp[MAX_SIZE][MAX_SIZE];
+    int i, j;
+
+
+    for (i = 0; i < size; i++) {
+        for (j = 0; j < size; j++) {
+            temp[i][j] = image[j][i];
+        }
+    }
+
+
+    for (i = 0; i < size; i++) {
+        for (j = 0; j < size; j++) {
+            image[i][j] = temp[i][size - 1 - j];
+        }
+    }
+
+    printf("Image rotated 90 degrees to the right successfully.\n");
+}
+
+
+void saveImage(int image[MAX_SIZE][MAX_SIZE], int size) {
+    FILE *file;
+    char filename[100];
+    int i, j;
+
+    printf("Enter the filename to save the edited image: ");
+    scanf("%s", filename);
+
+    file = fopen(filename, "w");
+    if (file == NULL) {
+        printf("Error opening file for writing.\n");
+        return;
+    }
+
+    fprintf(file, "%d\n", size);
+    for (i = 0; i < size; i++) {
+        for (j = 0; j < size; j++) {
+            fprintf(file, "%d ", image[i][j]);
+        }
+        fprintf(file, "\n");
+    }
+
+    fclose(file);
+}
+
 
 
